1f47c8faf39dc9b341a288a919844a484d269719
merwanel <>
2025-09-27 23:40:50 +0200

    1st test

---
+++ __tests__/PopUp.test.jsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import PopUp from '../src/app/PopUp'
+
+describe('PopUp', () => {
+  it('renders message correctly', () => {
+    const test_message = 'test message' ;
+    render(<PopUp message={test_message} />) ;
+
+    const message = screen.getByText(test_message) ;
+
+    expect(message).toBeInTheDocument() ;
+  })
+})
